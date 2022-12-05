597d821b35551b35bdde4bac0758c1b0e579828c
MoMo R <moran.risk78@gmail.com>
2022-12-05 14:18:58 -0800

    basic structure for frone-end

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+import Login from "./pages/Login";
+import Homepage from "./pages/Homepage";
+import Header from "./components/Header/";
+import Footer from "./components/Footer/";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div >
+      <Router basename="/">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Homepage />} /> 
+            <Route path="/Login" element={<Login />}/>
+          </Routes>
+          <Footer /> 
+        </Router>
+       </div>
   );
 }
 
---
+++ src/pages/Homepage/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./style.css"
+
+export default function Homepage() {
+  return (
+    <div className="homepage">
+        <h1>Home page! Welcome to K-pop trivia</h1>
+    </div>
+  )
+}
