52780521488cf4b117c8f829f9d993b777b207ec
LiyuanSilviaZhao <zhao0301liyuan@gmail.com>
2022-12-08 11:58:22 -0800

    wiki page

---
+++ src/pages/WikiPage/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./style.css"
+import { AnimatePresence } from "framer-motion";
+
+import  List  from "../../components/List";
+import  Item  from "../../components/Item";
+
+import { useParams } from "react-router-dom"
+
+
+export default function Wikipage() {
+  let { id } = useParams();
+  console.log("id is " + id);
+  const imageHasLoaded = true;
+
+  return (
+    <>
+    <div className='wikipage'>
+      <List selectedId={id} />
+      <AnimatePresence>
+        {id && imageHasLoaded && <Item id={id} key="item" />}
+      </AnimatePresence>
+      </div>
+    </>
+  );
+}
