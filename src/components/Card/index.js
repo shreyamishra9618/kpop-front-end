7e7e20a90fd6defc30812d59af3f9ad69efa8f47
michelle <113373744+mfarrell23@users.noreply.github.com>
2022-12-09 16:18:44 -0700

    adding api data

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,35 +1,4 @@
-// // import React, { useState } from "react";
-// import React from 'react'
-// import '../Card/style.css';
-// import { motion } from "framer-motion";
-// import { Link } from "react-router-dom";
 
-// export default function Card({ id, username, title, numberofLike, category, theme }) {
-//     return (
-//       <li className={`card ${theme}`}>
-//         <div className="card-content-container">
-//           <motion.div className="card-content" layoutId={`card-container-${id}`}>
-//             <motion.div
-//               className="card-image-container"
-//               layoutId={`card-image-container-${id}`}
-//             >
-//               <img className="card-image" src={`images/${id}.jpg`} alt="" />
-//             </motion.div>
-//             <motion.div
-//               className="title-container"
-//               layoutId={`title-container-${id}`}
-//             >
-//               <span className="category">{category}</span>
-//               <h2>{title}</h2>
-//               <div> Likes {numberofLike}</div>
-//               <h4>created by {username}</h4>
-//             </motion.div>
-//           </motion.div>
-//         </div>
-//         <Link to={id} className={`card-open-link`} />
-//       </li>
-//     );
-//   }
 
 
 import React from 'react'
@@ -38,7 +7,7 @@ import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 
 {/* Silvia- comment- need data - id */}
-export default function Card({ id, title }) {
+export default function Card({ id, title,Picture }) {
     return (
       <li className="card">
         <div className="card-content-container">
@@ -50,6 +19,9 @@ export default function Card({ id, title }) {
 {/* Silvia- comment- need data - id - feel free to change title/id to a better var name*/}
 
               <img className="card-image" src={`images/${id}.jpg`} alt="" />
+
+              <img className="card-image" src={Picture} alt="" />
+
             </motion.div>
             <motion.div
               className="title-container"
--- src/data.js
+++ src/data.js
@@ -1,3 +1,4 @@
+
 export const items = [
     {
       id: "a",
@@ -47,8 +48,74 @@ export const items = [
       numberofLike: 60,
       backgroundColor: "#282F49"
     },
-
+    {
+      id:"1",
+      GroupName:"BlackPink",
+      MemberNames:["Jenni Kim","Lalisa Manobal","Jisoo","Rosé"],
+      KoreanGroupName:"블랙핑크",
+      DateGroupFormed:2016,
+      Country:"Seoul,South Korea",
+      FandomName:" BLINK",
+      Instagram:"Instagram blackpinkofficial",
+      Top3Songs:["Ddu-DuDdu-Du","BOOMBAYAH","Whistle"],
+      Picture:"https://6.viki.io/image/6442cf3279fe465588ff07ee441c28f6/dummy.jpeg?s=900x600&e=t"
+      },
+      {
+      id:"2",
+      GroupName:"BTS",
+      MemberNames:["Jung Kook","V","Jin","SUGA","Jimin","RM"],
+      KoreanGroupName:"방탄소년단",
+      DateGroupFormed:"2010",
+      Country:"Seoul, South Korea",
+      FandomName:"ARMY",
+      Instagram:"bts.bighitofficial",
+      Top3Songs:["My Universe","Dynamite","I Need U."]
+      },
+      {
+      id:"3",
+      GroupName:"Twice",
+      MemberNames:["Nayeon","Momo","Tzuyu","Sana","Jeongyeon","Jihyo","Mina","Chaeyoung","Dahyun"],
+      KoreanGroupName:"트와이스",
+      DateGroupFormed:2015,
+      Country:"South Korea",
+      FandomName:"Once",
+      Instagram:"twicetagram",
+      Top3Songs:["Feel Special","Fancy","Cheer Up"]
+      },
+      {
+      id:"4",
+      GroupName:"iKon",
+      MemberNames:["Jinhwan","Bobby","Kim Donghyuk","KooJun-hoe","Song Yunhyeong","JungChanwoo"],
+      KoreanGroupName:"아이콘",
+      DateGroupFormed:2015,
+      Country:"Seoul,South Korea",
+      FandomName:"IKONIC",
+      Instagram:"withikonic",
+      Top3Songs:["Airplane","My Type","Apology"]
+      },
+      {
+      id:"5",
+      GroupName:"RedVelvet",
+      MemberNames:["Irene","Joy","Yeri","Wendy","Seulgi"],
+      KoreanGroupName:"레드벨벳",
+      DateGroupFormed:2014,
+      Country:"Seoul,SouthKorea",
+      FandomName:"ReVeluv",
+      Instagram:"redvelvet.smtown",
+      Top3Songs:["Psycho","FeelMyRhythm","BadBoy"]
+      }, 
+      {
+      id:"6",
+      GroupName:"BigBang",
+      MemberNames:["T.O.P","Taeyang","G-Dragon","Daesung"],
+      KoreanGroupName:"빅뱅",
+      DateGroupFormed:2006,
+      Country:"Seoul,SouthKorea",
+      FandomName:"V.I.P",
+      Instagram:"bigbang_official",
+      Top3Songs:["FXXKIT","HaruHaru","Loser"]
+      }
   ];
   
-  export const openSpring = { type: "spring", stiffness: 200, damping: 30 };
+  export const openSpring = { type: "spring", stiffness: 200, damping:30 };
   
