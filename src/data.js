4465e8d16c858b31b50a1cc122eabcb618cb8677
michelle <113373744+mfarrell23@users.noreply.github.com>
2022-12-11 16:17:02 -0700

    adding data to wiki page

--- src/data.js
+++ src/data.js
@@ -6,15 +6,24 @@ export const items = [
       username:"Silvia",
       title: "Blackpink Quiz",
       numberofLike: 80,
-      backgroundColor: "#814A0E"
+      backgroundColor: "#814A0E",
+      GroupName: "Black Pink has 4 members",
+      MemberNames: "Their Korean names are: Rosé 로제  ,Lisa 리사 ,Jennie 제니 and Jisoo 지수",
+      Country:"They have trained together for years learning to become an all around amazing group!",
+      Picture:"https://lastfm.freetls.fastly.net/i/u/770x0/d8fa3e34c1dc4fd4b87bb2372697d544.jpg#d8fa3e34c1dc4fd4b87bb2372697d544"
     },
     {
       id: "w",
-      category: "Blackpink",
+      category: "Kpop Boys",
       username:"Shreyas",
-      title: "All about BP",
+      title: "All about BZ Boys",
       numberofLike: 120,
-      backgroundColor: "#959684"
+      backgroundColor: "#959684",
+      GroupName:"BZ Boys",
+      MemberNames:"their korean names are: Bon 본 ,Taewoong 태웅 ,Hamin 그게 다야 ,Double D 더블디 and Seunghyn 승훈",
+      KoreanGroupName:"청공소년",
+      Country: "South Korea",
+      Picture:"https://i0.wp.com/kstationtv.com/wp-content/uploads/2021/08/BZ-BOYS-2.jpg?fit=600%2C400&ssl=1"
     },
     {
       id: "c",
@@ -22,31 +31,48 @@ export const items = [
       username:"Mo",
       title: "Lisa from BlackPink",
       numberofLike: 260,
-      backgroundColor: "#5DBCD2"
+      backgroundColor: "#5DBCD2",
+      MemberNames:"Lisa is the rapper of BlackPink",
+      Country:"She can speak Thai, English, And Korean",
+      FandomName:"Lisa is a brand ambassador for Moonshot cosmetics",
+      Picture:"https://assets.teenvogue.com/photos/635141b5bf33b9fca96cc1a0/16:9/w_2560%2Cc_limit/1434452878"
     },
     {
       id: "d",
       category: "BTS",
       username:"Mo",
-      title: "How much you know about BTS",
+      title: "How much you do know about BTS",
       numberofLike: 200,
-      backgroundColor: "#8F986D"
+      backgroundColor: "#8F986D",
+      Picture:"https://play-lh.googleusercontent.com/nUzK8WjxbT-X59mlDYyPej6HO_j45Ou7qOIIgnJbTO6IzNA6iAU_kMdA38hHGONBv9c",
+      MemberNames:"Their names in Korean are: Jung Kook 정국 V ,Jin 진 ,SUGA 슈가 ,Jimin 지민 and RM ",
+      GroupName:"They're known as Bangtan Boys",
+      Country:"They co-write and produce most of their own material"
     },
     {
       id: "f",
-      category: "BigBang",
+      category: "Kpop Girls",
       username:"Michelle",
-      title: "The BEST GROUP-BIGBANG",
+      title: "The BEST BlackPink girl",
       numberofLike: 150,
-      backgroundColor: "#FA6779"
+      backgroundColor: "#FA6779",
+      Picture:"https://img.kpopmap.com/680x384/2020/09/jennie-pink-hair-blackpink-ice-cream-uhdpaper.com-4K-7.2620-wp.thumbnail.jpg",
+      MemberNames:"Jennie Was born in South Korea, she studied in New Zealand for 5 years before returning and joining the group BlackPink.",
+      Country:"She was the 1st Korean celebrity to model for Boucheron Paris, a luxurious jewlery brand.",
+      GroupName:"Jennie loves to wear vintage 90's clothes"
     },
     {
       id: "e",
-      category: "BTS",
+      category: "Kpop Boys",
       username:"Silvia",
-      title: "BTS concerts",
+      title: "D1CE",
       numberofLike: 60,
-      backgroundColor: "#282F49"
+      backgroundColor: "#282F49",
+      Picture:"https://0.soompi.io/wp-content/uploads/2019/09/12003559/D1CE-1.jpg",
+      GroupName: "D1CE 디원스",
+      MemberNames:"Their names in Korean are: Yonggeun 용근 ,Hyunsoo 현수 ,Yoojin 유준 ,Woodam 우담 and Jinyoung 진영",
+      FandomName:"Don1y is their fan name",
+      Country:"South Korea"
     },
     {
       id:"1",
@@ -55,7 +81,7 @@ export const items = [
       KoreanGroupName:"블랙핑크",
       DateGroupFormed:2016,
       Country:"Seoul, South Korea",
-      FandomName:" BLINK",
+      FandomName:"BLINK is their fan name",
       Instagram:"Instagram blackpinkofficial",
       Top3Songs:["Ddu-DuDdu-Du"," BOOMBAYAH"," Whistle"],
       Picture:"https://6.viki.io/image/6442cf3279fe465588ff07ee441c28f6/dummy.jpeg?s=900x600&e=t"
@@ -67,7 +93,7 @@ export const items = [
       KoreanGroupName:"방탄소년단",
       DateGroupFormed:"2010",
       Country:"Seoul, South Korea",
-      FandomName:"ARMY",
+      FandomName:"ARMY is their fan name",
       Instagram:"Instagram bts.bighitofficial",
       Top3Songs:["My Universe"," Dynamite"," I Need U."],
       Picture:"https://i.scdn.co/image/ab6761610000e5eb5704a64f34fe29ff73ab56bb"
@@ -79,7 +105,7 @@ export const items = [
       KoreanGroupName:"트와이스",
       DateGroupFormed:2015,
       Country:"South Korea",
-      FandomName:"Once",
+      FandomName:"Once is their fan name",
       Instagram:"Instagram twicetagram",
       Top3Songs:["Feel Special"," Fancy"," Cheer Up"],
       Picture:"https://people.com/thmb/vKeU9x0JeCmBGBGQZ2dfXCE7_dU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(749x0:751x2)/twice86-5e1a7532b1244f10acfd8f1921e2fc08.jpg"
@@ -91,7 +117,7 @@ export const items = [
       KoreanGroupName:"아이콘",
       DateGroupFormed:2015,
       Country:"Seoul, South Korea",
-      FandomName:"IKONIC",
+      FandomName:"IKONIC is their fan name",
       Instagram:"Instagram withikonic",
       Top3Songs:["Airplane"," My Type"," Apology"],
       Picture:"https://www.billboard.com/wp-content/uploads/media/iKON-2018-cr-YG-Entertainment-billboard-1548.jpg"
@@ -103,7 +129,7 @@ export const items = [
       KoreanGroupName:"레드벨벳",
       DateGroupFormed:2014,
       Country:"Seoul, South Korea",
-      FandomName:"ReVeluv",
+      FandomName:"ReVeluv is their fan name",
       Instagram:"Instagram redvelvet.smtown",
       Top3Songs:["Psycho"," FeelMyRhythm"," BadBoy"],
       Picture:"https://www.allkpop.com/upload/2022/03/content/230242/1648017741-20220323-redvelvet.jpg"
@@ -115,7 +141,7 @@ export const items = [
       KoreanGroupName:"빅뱅",
       DateGroupFormed:2006,
       Country:"Seoul, South Korea",
-      FandomName:"V.I.P",
+      FandomName:"V.I.P is their fan name",
       Instagram:"Instagram bigbang_official",
       Top3Songs:["FXXKIT"," HaruHaru"," Loser"],
       Picture:"https://www.allkpop.com/upload/2021/08/content/190018/1629346688-20200609-bigbang.jpg"
