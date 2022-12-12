26e8a6c11f0c2a0870c0c0dc92fdb82f67b6efa4
michelle <113373744+mfarrell23@users.noreply.github.com>
2022-12-12 13:01:01 -0700

    adding styl and more info to wiki

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,10 +1,8 @@
-
 import React from 'react'
 import '../Card/style.css';
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 
-{/* Silvia- comment- need data - id */}
 export default function Card({ id, title,Picture }) {
     return (
       <li className="card">
@@ -14,7 +12,6 @@ export default function Card({ id, title,Picture }) {
               className="card-image-container"
               layoutId={`card-image-container-${id}`}
             >
-{/* Silvia- comment- need data - id - feel free to change title/id to a better var name*/}
 
               <img className="card-image" src={`images/${id}.jpg`} alt="" />
 
@@ -25,7 +22,6 @@ export default function Card({ id, title,Picture }) {
               className="title-container"
               layoutId={`title-container-${id}`}
             >
-              {/* Silvia- comment- data - group name (title)- feel free to change title to a better var name*/}
               <h1 className='title'>{title}</h1> 
             </motion.div>
           </motion.div>
